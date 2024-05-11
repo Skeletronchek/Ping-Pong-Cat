785008c4a87774539f4e024d27c9e0e8ef851168
Skeletronchek <162158420+Skeletronchek@users.noreply.github.com>
2024-05-11 18:20:17 +0300

    main.py

--- main.py
+++ main.py
@@ -30,11 +30,19 @@ class Player(GameSprite):
     def update(self):
         global cd
         global starttime
+        global lost
+        global finish
         keys = key.get_pressed() 
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speedx
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speedx
+        if keys[K_1] and lost == 'nigla':            
+            finish = False
+            lost = False
+        if keys[K_2] and lost == 'nigla':            
+            finish = False
+            lost = 'ninga'
 
 
 class Ball(GameSprite):
@@ -77,10 +85,19 @@ class Enemy(GameSprite):
     def update(self):
         global cd
         global starttime
-        if Ball.rect.x < self.rect.x:
-            self.rect.x -= self.speedx
-        if Ball.rect.x > self.rect.x:
-            self.rect.x += self.speedx
+        global lost
+        if lost == 'ninga':
+            keys = key.get_pressed() 
+            if keys[K_a] and self.rect.x > 5:
+                self.rect.x -= self.speedx
+            if keys[K_d] and self.rect.x < win_width - 80:
+                self.rect.x += self.speedx
+        elif lost == False:
+            if ball.rect.x < self.rect.x:
+                self.rect.x -= self.speedx
+            if ball.rect.x > self.rect.x:
+                self.rect.x += self.speedx
+
 
 
 
@@ -99,12 +116,14 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 display.set_caption("Cat Tennis")
 
 Players = sprite.Group()
-
 isac = Player(img_hero, win_width /2, win_height - 80, 60, 60, 6,0)
 cat = Enemy(img_enemy, win_width /2, 0, 60, 60, 6,0)
-Ball = Ball(img_ball, win_width /2, win_height /2, 60, 60, 5.4,5.4)
 Players.add(isac)
 Players.add(cat)
+ball = Ball(img_ball, win_width /2, win_height /2, 60, 60, 5.4,5.4)
+
+
+
 
 
 
@@ -113,10 +132,10 @@ Players.add(cat)
 
 
 run = True
-finish = False
+finish = True
 clock = time.Clock()
 FPS = 60
-
+lost = 'nigla'
 
 #музыка
 mixer.init()
@@ -132,6 +151,13 @@ while run:
             run = False
                 
     window.blit(background,(0,0))
+    textlost = font2.render('CAT WIN!', 1, (0, 255, 0))
+    textwin = font2.render('ISAC WIN!', 1, (0, 255, 0))
+    textname = font2.render('Racist Cat Ping Pong', 1, (255, 255, 255))
+    textlol = font2.render('Press 1 to PvE', 1, (255, 255, 255))
+    textlol2 = font2.render('Press 2 to PvP', 1, (255, 255, 255))
+    isac.update()
+    isac.reset()
     if not finish:
         if cd == True:
             curtime = timer()
@@ -142,14 +168,28 @@ while run:
                 delay = 0
                 starttime = 0
                 curtime = 0
-        if Ball.rect.y > win_height - 70 or Ball.rect.y < -5:
+        if ball.rect.y > win_height - 70 or ball.rect.y < -5:
             finish = True
-        isac.update()
+            if ball.rect.y < -5:
+                lost = False
+            elif ball.rect.y > win_height - 70:
+                lost = True
+
         cat.update()
-        Ball.update()
-        isac.reset()
-        Ball.reset()
+        ball.update()
+        ball.reset()
         cat.reset()
-        display.update()   
-           
+    else:
+        if lost == True:
+            window.blit(textlost, (win_height/2, win_width/3))
+        elif lost == False:
+            window.blit(textwin, (win_height/2, win_width/3))
+        else:
+            window.blit(textname, (win_height/3, win_width/4.5))
+            window.blit(textlol, (win_height/3, win_width/2))
+            window.blit(textlol2, (win_height/3, win_width/3))
+
+          
+    display.update()      
     time.delay(30)
+
